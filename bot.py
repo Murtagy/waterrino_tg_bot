1a01597cd50fe2b57168c33deaaa33bab44fc27a
Maksim R <maksim@dohop.com>
2022-12-19 22:45:16 +0300

    too big drink

--- bot.py
+++ bot.py
@@ -161,6 +161,8 @@ async def drink_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
             await update.message.reply_text(
                 "Too much for a single drink. Single drink should be below 2000"
             )
+            return ConversationHandler.END
+
         drink = Drink(user_id=db_user.user_id, mililitres=mililitres)
         session.add(drink)
         await session.flush()
